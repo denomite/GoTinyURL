7d6c7fc2a524b12c5e8edf2180c047a353ac8b6e
Denis Soke <sokedenis@gmail.com>
2025-04-13 19:53:03 +0200

    added base context, redis connect and mux router

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"context"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/redis/go-redis/v9"
+)
+
 func main() {
+	ctx := context.Background()
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: "redis:6379",
+	})
+
+	// interal code !
+
+	r := mux.NewRouter()
+
+	log.Println("Server started on :8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 
 }
